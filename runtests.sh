360439ecf760ce35146132f312b0e53545b08d25
Chuck Piercey <chuck.piercey@tidalscale.com>
2015-10-21 10:50:44 -0700

    Update to final production test

--- runtests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo swapoff -a
-for x in 1 1 3 5 8 10 13 17 20
-do
-        echo Starting size $x 
-	/usr/bin/time --format="\nMax mem:%M kilobytes\n" R --no-save -q --slave < cms_data_analysis.R --args 1 $x /data/cms False >> testoutput.txt
-done
