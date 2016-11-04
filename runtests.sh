1f2c00f3e48006c227ceddd66b6d4504052a6991
Chuck Piercey <cpiercey@icloud.com>
2016-11-04 10:10:25 -0700

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,15 +1,8 @@
-host=$(hostname)
-cpus=$(grep -c ^processor /proc/cpuinfo)
-echo "Running on $host with $cpus cpus and $total_mem_in_gb GB"
-mkdir -p $HOME/benchmark-results
-RESULTS="$HOME/benchmark-results/${host}-${cpus}-${total_mem_in_gb}GB"
-mkdir -p $(dirname $RESULTS)
-cd ~/tests/R-test/cms_test
-# Start test
-# Run on five 128GB nodes (i.e. a 640GB guest)
-for x in  0 1 3 5 8 10 13 17
+# Chuck Piercey
+#!/bin/bash
+sudo swapoff -a
+for x in 1 5 10 17 20
 do
-	        echo Starting sample size $x
-		        SAMPLE_RESULTS="$RESULTS-samples-$x"
-			        nosetests -v cms_test.py -s -a "samples=$x,function=all" --with-json --json-file=$SAMPLE_RESULTS.json 2>&1 > $SAMPLE_RESULTS.log
+        echo Starting size $x
+        /usr/bin/time --format="\nMax mem:%M kilobytes\n" R --no-save -q --slave < cms_data_analysis.R --args 1 $x /data/cms False >> testoutput.txt
 done
