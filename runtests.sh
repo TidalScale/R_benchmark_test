b747b2853145bbddf11677154b8b650ad396cbf3
tidalscale-jamon <jamon.bowen@tidalscale.com>
2017-04-04 16:37:39 -0600

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,7 @@
 # Chuck Piercey
 #!/bin/bash
 sudo swapoff -a
+#the Aproximate in memory size is 34 GB * the x value below (i.e. 20*34 = 640 GB).
 for x in 1 5 10 17 20
 do
         echo Starting size $x
