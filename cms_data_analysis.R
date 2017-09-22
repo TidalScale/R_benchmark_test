ae4d90be2e1fe402fd221ad5eaba7f675fabe87b
Chuck Piercey <cpiercey@icloud.com>
2017-09-22 08:19:05 -0700

    Update cms_data_analysis.R

--- cms_data_analysis.R
+++ cms_data_analysis.R
@@ -1,6 +1,6 @@
 # Set Dec 1, 2015 as the reference date,
 # cf. http://mran.revolutionanalytics.com/web/packages/checkpoint/vignettes/checkpoint.html
-library(checkpoint)  # comment this line out after running this test the first time
+library(checkpoint)  # comment these 2 lines out after running this test the first time
 checkpoint("2015-12-01")
 
 # Clear the memory before running
